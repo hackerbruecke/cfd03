88f3a110da4d24d05616ea729a978c5dabb2d620
Andre Malcher <andre.malcher@TUM>
2014-05-17 21:10:14 +0200

    Replaced direct computations of index by call to idx() function.

--- boundary.c
+++ boundary.c
@@ -1,4 +1,3 @@
-/* TODO: Boundary conditions must be rewritten (should work with arbitrary cube sizes) */
 #include "boundary.h"
 #include "LBDefinitions.h"
 #include "computeCellValues.h"
@@ -42,9 +41,8 @@ void treatBoundary(double *collideField, int* flagField,
 
 						/*Inverse lattice velocity to c_i is accessed
 						at Q-i-1-th position of (x+dx, y+dy, z+dz) cell*/
-
-						finv = collideField[Q * ((z+dz)*(nx+2)*(ny+2) + (y+dy)*(nx+2) + x+dx) + Q-i-1];
-						collideField[Q * (z*(nx+2)*(ny+2) + y*(nx+2) + x) + i] = finv;
+						finv = collideField[idx(xlength, x+dx, y+dy, z+dz, inv(i))];
+						collideField[idx(xlength, x, y, z, i)] = finv;
 					}
 					/*x and y swapped, so that another pair of boundary planes may be accessed within the same iteration*/
 					/*x-z plane*/
@@ -52,8 +50,8 @@ void treatBoundary(double *collideField, int* flagField,
 						x+dy > 0 && x+dy < ny + 1 &&
 						z+dz > 0 && z+dz < nz + 1) {
 
-						finv = collideField[Q * ((z+dz)*(nx+2)*(ny+2) + (x+dy)*(nx+2) + y+dx) + Q-i-1];
-						collideField[Q * (z*(nx+2)*(ny+2) + x*(ny+2) + y) + i] = finv;
+						finv = collideField[idx(xlength, y+dx, x+dy, z+dz, inv(i))];
+						collideField[idx(xlength, y, x, z, i)] = finv;
 					}
 					/*x and z swapped, out of same reason*/
 					/*x-y plane*/
@@ -61,12 +59,12 @@ void treatBoundary(double *collideField, int* flagField,
 						y+dy > 0 && y+dy < ny + 1 &&
 						x+dz > 0 && x+dz < nz + 1) {
 
-						finv = collideField[Q * ((x+dz)*(nx+2)*(ny+2) + (y+dy)*(nx+2) + z+dx) + Q-i-1];
+						finv = collideField[idx(xlength, z+dx, y+dy, x+dz, inv(i))];
 
 						/*Additional term when we are considering moving-wall (top plane - k=1)*/
 						/*Velocity of the moving-wall is taken into account*/
 						if(k == 1) {
-							currentCell = collideField + Q * ((x+dz)*(nx+2)*(ny+2) + (y+dy)*(nx+2) + z+dx);
+							currentCell = collideField + idx(xlength, z+dx, y+dy, x+dz, 0);
 							computeDensity(currentCell, &density);
 							cu = 0; /*dot product of c_i and velocity of wall*/
 							for (int d = 0; d < D; ++d) {
@@ -74,7 +72,7 @@ void treatBoundary(double *collideField, int* flagField,
 							}
 							finv += 2*LATTICEWEIGHTS[i]*density*cu/(C_S*C_S);
 						}
-						collideField[Q * (x*(nx+2)*(ny+2) + y*(nx+2) + z) + i] = finv;
+						collideField[idx(xlength, z, y, x, i)] = finv;
 					}
 				}
 			}
